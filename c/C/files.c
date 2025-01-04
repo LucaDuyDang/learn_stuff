01e7ba5b857bba605f9bbb1b424608c69d4b5715
luca <danghoangduy120300@gmail.com>
2025-01-04 14:42:35 +0700

    final-cm files.c

--- c/C/files.c
+++ c/C/files.c
@@ -2,6 +2,12 @@
 
 int main()
 {
+    // syntax for create a files txt
+    /*
+    w - Writes to a file
+    a - Appends new data to a file
+    r - Reads from a file
+    */
     FILE *file;
     file = fopen("test.txt", "w");
 
