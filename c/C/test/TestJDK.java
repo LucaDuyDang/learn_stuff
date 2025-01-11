aeadb84552b67057404c7d0266ae889589714604
luca <danghoangduy120300@gmail.com>
2025-01-11 15:05:49 +0700

    u-install-javajdk17

---
+++ c/C/test/TestJDK.java
@@ -0,0 +1,8 @@
+
+public class TestJDK {
+
+    public static void main(String[] args) {
+        // Print a simple message to test JDK setup
+        System.out.println("Hello, Luca! Your JDK is working perfectly.");
+    }
+}
