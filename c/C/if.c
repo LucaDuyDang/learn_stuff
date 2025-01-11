f9def9e83c52cf0d0bc38ddc06312f98c962430b
luca <danghoangduy120300@gmail.com>
2025-01-11 15:48:16 +0700

    u-if

--- c/C/if.c
+++ c/C/if.c
@@ -3,5 +3,16 @@
 int main()
 {
     // programing nested;
+    int n, i, j, sum;
+    printf("Type Input N : \n ");
+    scanf("%d", &n);
+
+    sum = 0;
+    for (i = 1; i <= n; i++)
+    {
+        sum += n;
+    }
+    printf("Sum is : %d ", sum);
+
     return 0;
 }
