9267cd631fd15be9f8d120a0142f465d2f24057d
luca <danghoangduy120300@gmail.com>
2025-03-15 20:54:49 +0700

    last update before use linux operation

---
+++ c/C/homework/hwFour.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+fuctionOne()
+{
+    return 0;
+}
+
+int main()
+{
+    return 0;
+}
+
+functionOne()
+{
+    int a = 1;
+
+    int i = a;
+    int sum = a + i;
+
+    for (int j = 0; j <= 5; j++)
+    {
+        scanf("%d", &a);
+    }
+}
