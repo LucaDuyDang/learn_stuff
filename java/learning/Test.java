e21f357b0bcc3e203061afffa6aaa35832522d59
luca <danghoangduy120300@gmail.com>
2025-05-06 18:19:08 +0700

    update 06.05

--- java/learning/Test.java
+++ java/learning/Test.java
@@ -1,8 +1,3 @@
-package learning;
-
 public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("This is a test Java program.");
-    }    
+    
 }
